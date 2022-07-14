070ccb7ce04e444fbb74f01288cb9486b5ba3f57
sunflowerr3 <109298023+sunflowerr3@users.noreply.github.com>
2022-07-14 19:46:03 +0300

    Add files via upload

---
+++ задача 8.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+int number = 30;
+int count = 2;
+
+while (count <= number)
+{
+    Console.WriteLine(count + " ");
+    count += 2;
+}
+Console.WriteLine();
